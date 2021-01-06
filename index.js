f828d105a0f3bea65ed048e6cc5f2800144d9813
Use your GitHub username <Enter your email address>
2021-01-07 05:14:23 +0800

    update

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let update = "Last Updated:";
 let t = document.lastModified;
 let fulldate = update + " " + t ;
 
-document.getElementById("Date").textContent = fulldate;
+document.getElementById("Date").innerHTML = fulldate;
