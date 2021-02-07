99d71383903db7a47154ceeebd600a1b205f38fb
Use your GitHub username <Enter your email address>
2021-02-07 20:40:14 +0800

    initial commit

--- weather/weather.js
+++ weather/weather.js
@@ -47,3 +47,28 @@ document.getElementById("Greetings").innerHTML = "Preston Pancakes in the Park! 
 " " + "at the city park pavillion."
 + " ";
 // get the name of the week as string and not number
+
+
+
+/*---------WEATHER-------*/
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+.then(function(response) {
+    return response.json();
+})
+.then(function (jsonObject) {
+    console.table(jsonObject); //temporary checking for valid response and data parsing
+})
+
+const towns = jsonObject['towns'];
+
+for (let i = 0; i < towns.length; i++ ); //{(how would you do this with a foreach method?)
+
+let card = document.createElement('section');
+let h2 = document.createElement('h2');
+h2.textContent = towns[i].name + ' ' + towns[i].lastname;
+card.appendChild(h2);
+document.querySelector('div.towns').appendChild(card);
+
+image.setAttribute('src', towns[i].imageurl);
