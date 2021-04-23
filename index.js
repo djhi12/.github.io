4b443b86fe9c16e7f94717a51e49824351a42d01
Daniel John Idorot <djhi_lds@yahoo.com>
2021-04-24 05:39:02 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const links = [
     {
       label: "Week1 notes",
-      url: "front_end_2_html/index.html"
+      url: "front_end_2_html/week_1.html",
+    
+      label: "Week2 notes",
+      url: "front_end_2_html/week_2.html"
+    
     }
   ]
 
