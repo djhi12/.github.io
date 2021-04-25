00d6ff850f4d794d6d573a6cb9e7ce48aa13f318
Daniel John Idorot <63905632+djhilds12@users.noreply.github.com>
2021-04-25 11:26:42 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const links = [
-    {
-      label: "Week1 notes",
-      url: "front_end_2_html/week_1.html",
-    
-      label: "Week2 notes",
-      url: "front_end_2_html/week_2.html"
-    
-    }
+  {
+  label: "Week1 notes",
+  url: "front_end_2_html/week_1.html",
+  },
+  {
+  label: "Week2 notes",
+  url: "front_end_2_html/week_2.html"
+  }
+
   ]
 
-  document.getElementById("Links").innerHTML = links;
+document.getElementById("Links").innerHTML = links[0].label;
+document.getElementById("Links").innerHTML += links[1].label;
+
   console.log("Links");
