8f437d9422689de2846e845d70e95d11b6173515
djhi12 <ido20002@byui.edu>
2021-07-12 21:11:27 +0800

    initial commit

--- front_end_2_js/hikes.js
+++ front_end_2_js/hikes.js
@@ -1,4 +1,4 @@
-import Comments from './comments.js';
+import Comments from '/front_end_2_js/comments.js';
 // Example of using Classes and modules to organize the code needed to render our list of hikes. Not using MVC here.
 
 //create an array of hikes
