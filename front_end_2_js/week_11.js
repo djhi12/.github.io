27ab1adb4300f80bc04865abef17461d34d4b0c6
djhi12 <ido20002@byui.edu>
2021-07-15 22:34:53 +0800

    initial commit

--- front_end_2_js/week_11.js
+++ front_end_2_js/week_11.js
@@ -1,3 +1,35 @@
+
+
+let daynames = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+  ]
+  
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
+  
+  let d = new Date();
+  let dayName = daynames[d.getDay()];
+  let monthName = months[d.getMonth()];
+  let year = d.getFullYear();
+  let fulldate = dayName + ", " + d.getDate() + " " + monthName + ", " + year;
+  
+  document.getElementById("Date").innerHTML = fulldate;
+
+
+/********************************************* */
+
 /*
 Reduce()
 1. The reduce() method is another method that iterates over each value in the array, but this time it 
--- front_end_2_js/week_9.js
+++ front_end_2_js/week_9.js
@@ -1,3 +1,34 @@
+
+let daynames = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+  ]
+  
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
+  
+  let d = new Date();
+  let dayName = daynames[d.getDay()];
+  let monthName = months[d.getMonth()];
+  let year = d.getFullYear();
+  let fulldate = dayName + ", " + d.getDate() + " " + monthName + ", " + year;
+  
+  document.getElementById("Date").innerHTML = fulldate;
+
+
+/****************************************************************** */
+
 //Continuation of Chapter 3 | Arrays, Logic, and Loops
 
 /*
@@ -411,31 +442,3 @@ function mean_1(...values) {
 mean_1(2,8,13,11,4,2);
 
 
-/********************************************************* */
-
-let daynames = [
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
-  ]
-  
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ]
-  
-  let d = new Date();
-  let dayName = daynames[d.getDay()];
-  let monthName = months[d.getMonth()];
-  let year = d.getFullYear();
-  let fulldate = dayName + ", " + d.getDate() + " " + monthName + ", " + year;
-  
-  document.getElementById("Date").innerHTML = fulldate;
