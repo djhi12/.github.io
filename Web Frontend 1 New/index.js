bc2d9ad3be209172530e3e14cd044efd312ccc8d
djhi12 <ido20002@byui.edu>
2021-12-11 18:41:10 +0800

    initial commit

---
+++ Web Frontend 1 New/index.js
@@ -0,0 +1,13 @@
+/******* Date *******/
+const dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const createDate =  new Date();
+c
+
+
+// const dayName = dayNames.getDay();
+// const fullDate = monthName + dayName;
+// console.log(fullDate);
+
+
+console.log("Daniel");
