09fdc924fddd43ffb2b3182f9e7d727e828a3b68
djhi12 <ido20002@byui.edu>
2021-12-16 19:35:34 +0800

    initial commit

---
+++ Javascript Novice to Ninja 2nd Edition New Folder/Chapter 6 - The Document Object Model/index.js
@@ -0,0 +1,17 @@
+console.log("Hello world!");
+
+/******* What is the DOM? *******/
+
+
+/****** History of the DOM *******/
+// ------ Sample from w3schools
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.border = "10px solid green";
+    document.getElementById("demo").style.borderRadius = "10px";
+    document.getElementById("demo").style.padding = "10px";
+    document.getElementById("demo").style.textAlign = "center";
+    document.getElementById("demo").classList.toggle("mystyle");
+    
+  }
