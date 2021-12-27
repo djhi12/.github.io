a3bd9abd582f5acaf84273ba77479411e64748de
djhi12 <ido20002@byui.edu>
2021-12-27 23:09:50 +0800

    initial commit

--- Javascript Novice to Ninja 2nd Edition New Folder/Chapter 7 - Events/index.js
+++ Javascript Novice to Ninja 2nd Edition New Folder/Chapter 7 - Events/index.js
@@ -17,8 +17,44 @@ document.onclick = function (){
 // document.body.addEventListener('click', doSomething);
 // addEventListener('click', () => alert('You click'));
 
+/*
 function doSomething() {
-    document.body.classList.toggle("mystyle");
+   // document.body.classList.toggle("mystyle");
+   console.log('Something Happened!');
 }
 
 addEventListener('click', doSomething);
+*/
+
+/****** Example Code *******/
+function doSomething(e) {
+    // document.body.classList.toggle("mystyle");
+    console.log(e.type);
+ }
+ 
+ addEventListener('click', doSomething);
+
+/******* The Event Object *******/
+
+
+/****** Types of Event *******/
+
+
+/******* The Event Target *******/
+function doSomething1(e1) {
+    // document.body.classList.toggle("mystyle");
+    console.log(e1.target);
+ }
+ 
+ addEventListener('click', doSomething1);
+
+
+ /****** Coordinates of an Event *******/
+
+
+
+
+
+
+
+
