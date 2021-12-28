f0de89aac15e1ee9b19f1242afdb973ff1eef54b
djhi12 <ido20002@byui.edu>
2021-12-28 22:49:08 +0800

    initial commit

--- Javascript Novice to Ninja 2nd Edition New Folder/Chapter 7 - Events/index.js
+++ Javascript Novice to Ninja 2nd Edition New Folder/Chapter 7 - Events/index.js
@@ -52,8 +52,27 @@ function doSomething1(e1) {
  /****** Coordinates of an Event *******/
 
 
+ /******** Types of Events ********/
+// Mouse Events
+const clickParagraph = document.getElementById('click');
+
+clickParagraph.addEventListener('click', () => console.log('click'));
+clickParagraph.addEventListener('mousedown', () => console.log('down'));
+clickParagraph.addEventListener('mouseup', () => console.log('up'));
 
 
+// ondblclick Event
+const dblclickParagraph = document.getElementById('dblclick');
+dblclickParagraph.addEventListener('dblclick', highlight);
+
+function highlight(event) {
+    event.target.classList.toggle('highlight');
+}
+
+// mouseover event
+const mouseParagraph = document.getElementById('mouse');
+mouseParagraph.addEventListener('mouseover', highlight);
+mouseParagraph.addEventListener('mouseout', highlight);
 
 
 
