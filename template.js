d8eb68c3016eacf5ab586b9d848c8c9795cd4120
Daniel John Idorot <ido20002@byui.edu>
2022-04-23 19:46:52 +0800

    initial commit

---
+++ template.js
@@ -0,0 +1,13 @@
+
+// Menu Bar
+let menuBar = document.querySelector('#menuBar');
+let navList = document.querySelector('#navList');
+
+menuBar.addEventListener('click', ()=>{
+  navList.classList.toggle('reveal');
+})
+
+console.log('The code is working');
+
+
+
