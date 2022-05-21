a5397bd45beb6464e967425a5aa83be1e1628198
Daniel John Idorot <ido20002@byui.edu>
2022-05-21 18:41:23 +0800

    initial commit

--- python/text_based.py
+++ python/text_based.py
@@ -2,15 +2,16 @@
 
 # scenario = print('You are walking through a dark forest ')
 
-print(f"You are walking through a dark forest and find two item: " + "a MATCH and a FLASHLIGHT. " + "Which one do you want to pick up? ")
+item = input(f"You are walking through a dark forest and find two item: " + "a MATCH and a FLASHLIGHT. " + "Which one do you want to pick up? ")
 
 # Match
-item = input('Enter item: ')
+# item = input('Enter item: ')
 
+# scenario for match
 if item == "match": 
     match = input("You pick up the match and strike it, and for an instant, the forest around you is illuminated. You see a large grizzly bear, and then the match burns out. Do you want to RUN, or HIDE behind a tree?") 
 
-    print()
+    print() # space
 
     if match == "run":
         print("Run run run")
@@ -22,14 +23,13 @@ if item == "match":
 if item == "flashlight":
     flashlight = input("You pick up the flashlight and turn it on. You see the pathway lit up in front of you, but you thought you also heard something off to the side. Do you want to FOLLOW the path or LOOK in the trees for the thing that made the noise?") 
 
-    print()
+    print() # space
 
     if flashlight == "follow":
         print("Follow follow follow")
 
     elif flashlight == "look":
         print("Look look look")
-
 
 
 
