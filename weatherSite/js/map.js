89da0c457947207345f3a908f085bd42df269503
Daniel John Idorot <ido20002@byui.edu>
2022-06-11 21:07:56 +0800

    initial commit

---
+++ weatherSite/js/map.js
@@ -0,0 +1,16 @@
+
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&v=weekly"
+defer> 
+// let map = ;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 8,
+    });
+}
+
+window.initMap = initMap;
