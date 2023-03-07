6c9fce3b2d6000b14dd41f6b7d57d60de7078d14
Daniel John Idorot <ido20002@byui.edu>
2023-03-07 10:41:31 +0800

    Initial commit

---
+++ JavaScriptLanguage/Week3/scripts/task3.js
@@ -0,0 +1,149 @@
+/* Lesson 3 */
+
+/* FUNCTIONS */
+
+// Step 1: Using function declaration, define a function named add that takes two arguments, number1 and number2
+
+// Step 2: In the function, return the sum of the parameters number1 and number2
+
+// Step 3: Step 3: Using function declaration, define another function named addNumbers that gets the values of two HTML form controls with IDs of addend1 and addend2. Pass them to the add function
+
+// Step 4: Assign the return value to an HTML form element with an ID of sum
+
+// Step 5: Add a "click" event listener to the HTML button with an ID of addNumbers that calls the addNumbers function
+
+// Step 6: Using function expressions, repeat Steps 1-5 with new functions named subtract and subtractNumbers and HTML form controls with IDs of minuend, subtrahend, difference and subtractNumbers
+
+// Step 7: Using arrow functions, repeat Steps 1-5 with new functions named multiply and mulitplyNumbers and HTML form controls with IDs of factor1, factor2, product and multiplyNumbers
+
+// Step 8: Using any of the three function declaration types, repeat Steps 1-5 with new functions named divide and divideNumbers and HTML form controls with IDs of dividend, divisor, quotient and divideNumbers
+
+// Step 9: Test all of the mathematical functionality of the task3.html page.
+
+
+/* BUILT-IN METHODS */
+
+// Step 1: Declare and instantiate a variable of type Date to hold the current date
+
+// Step 2: Declare a variable to hold the current year
+
+// Step 3: Using the variable declared in Step 1, call the built-in getFullYear() method/function and assign it to the variable declared in Step 2
+
+// Step 4: Assign the current year variable to an HTML form element with an ID of year
+
+
+/* ARRAY METHODS */
+
+// Step 1: Declare and instantiate an array variable to hold the numbers 1 through 25
+
+// Step 2: Assign the value of the array variable to the HTML element with an ID of "array"
+
+// Step 3: Use the filter array method to find all of the odd numbers of the array variable and assign the reult to the HTML element with an ID of "odds" ( hint: % (modulus operartor) )
+
+// Step 4: Use the filter array method to find all of the even numbers of the array variable and assign the result to the HTML element with an ID of "evens"
+
+// Step 5: Use the reduce array method to sum the array variable elements and assign the result to the HTML element with an ID of "sumOfArray"
+
+// Step 6: Use the map array method to multiple each element in the array variable by 2 and assign the result to the HTML element with an ID of "multiplied"
+
+// Step 7: Use the map and reduce array methods to sum the array elements after multiplying each element by two.  Assign the result to the HTML element with an ID of "sumOfMultiplied"
+
+
+// Addition
+function add(number1, number2) {
+    return Number(number1) + Number(number2);
+  }
+  
+  function addNumbers() {
+    const addEndOne = document.getElementById("addend1").value;
+    const addEndTwo = document.getElementById("addend2").value;
+  
+    const sum = add(addEndOne, addEndTwo);
+  
+    document.getElementById("sum").value = sum;
+  }
+  
+  document.getElementById("addNumbers").addEventListener("click", addNumbers);
+  
+  
+  // Subtraction
+  function subtract(number1, number2) {
+    return Number(number1) - Number(number2);
+  }
+  
+  function subtractNumbers() {
+    const subEndOne = document.getElementById("minuend").value;
+    const subEndTwo = document.getElementById("subtrahend").value;
+  
+    const subNum = subtract(subEndOne, subEndTwo);
+  
+    document.getElementById("difference").value = subNum;
+  }
+  
+  document.getElementById("subtractNumbers").addEventListener("click", subtractNumbers);
+  
+  
+  // Multiplication
+  const multiply = (number1, number2) => Number(number1) * Number(number2);
+  
+  function multiplyNumbers() {
+    const factorOne = document.getElementById("factor1").value;
+    const factorTwo = document.getElementById("factor2").value;
+  
+    const product = multiply(factorOne, factorTwo);
+  
+    document.getElementById("product").value = product;
+  }
+  
+  document.getElementById("multiplyNumbers").addEventListener("click", multiplyNumbers);
+  
+  
+  // Division
+  const divide = (number1, number2) => Number(number1) / Number(number2);
+  
+  function divideNumbers() {
+    const dividend = document.getElementById("dividend").value;
+    const divisor = document.getElementById("divisor").value;
+  
+    const quotient = divide(dividend, divisor);
+  
+    document.getElementById("quotient").value = quotient;
+  }
+  
+  document.getElementById("divideNumbers").addEventListener("click", divideNumbers);
+  
+  
+  // Array Methods
+  const sourceArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+  
+  const oddNumbers = sourceArray.filter(function (num) {
+    return num % 2 !== 0;
+  });
+  
+  const evenNumbers = sourceArray.filter(function (num) {
+    return num % 2 === 0;
+  });
+  
+  const sumOfArray = sourceArray.reduce(function (acc, num) {
+    return acc + num;
+  }, 0);
+  
+  const multipliedArray = sourceArray.map(function (num) {
+    return num * 2;
+  });
+  
+  const sumOfMultiplied = multipliedArray.reduce(function (acc, num) {
+    return acc + num;
+  }, 0);
+  
+  document.getElementById("array").innerHTML = sourceArray;
+  document.getElementById("odds").innerHTML = oddNumbers;
+  document.getElementById("evens").innerHTML = evenNumbers;
+  document.getElementById("sumOfArray").innerHTML = sumOfArray;
+  document.getElementById("multiplied").innerHTML = multipliedArray;
+  document.getElementById("sumOfMultiplied").innerHTML = sumOfMultiplied;
+  
+  
+  // Year in Footer
+  const year = new Date().getFullYear();
+  document.getElementById("year").innerHTML = year;
