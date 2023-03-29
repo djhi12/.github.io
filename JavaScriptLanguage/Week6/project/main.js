bc82dd0daee21dd21ba03b6840db5f05ecac543f
Daniel John Idorot <ido20002@byui.edu>
2023-03-29 18:30:09 +0800

    Initial commit

--- JavaScriptLanguage/Week6/project/main.js
+++ JavaScriptLanguage/Week6/project/main.js
@@ -11,11 +11,13 @@ function addLog() {
     }
 }
 
+// Delete log
 function deleteLog(index) {
     logs.splice(index, 1);
     updateLogList();
 }
 
+// Update log 
 function updateLogList() {
     const logList = document.getElementById("logList");
     logList.innerHTML = "";
@@ -24,19 +26,30 @@ function updateLogList() {
         const li = document.createElement("li");
         li.appendChild(document.createTextNode(log));
 
+        // Delete
         const deleteButton = document.createElement("button");
         deleteButton.appendChild(document.createTextNode("Delete"));
         deleteButton.addEventListener("click", function () {
             if (confirm("Are you sure you want to delete this log?")) {
                 deleteLog(index);
             }
-            deleteButton.setAttribute("class", "deleteButton");
         });
 
-
+        // Edit
+        const editButton = document.createElement("button");
+        editButton.appendChild(document.createTextNode("Edit"));
+        editButton.addEventListener("click", function () {
+            const newLog = prompt("Enter new log:", log);
+            if (newLog !== null) {
+                logs[index] = newLog.trim();
+                updateLogList();
+            }
+        });
+        li.appendChild(editButton);
         li.appendChild(deleteButton);
+        
         logList.appendChild(li);
         deleteButton.classList.add("delete-button");
+        editButton.classList.add("edit-button");
     });
-
 }
