17c2e538e87fc20173062ef6cc31ed220fa4f4ca
djhi12 <ido20002@byui.edu>
2023-08-24 14:19:44 +0800

    Initial commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,40 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { title: 'Car Insurance', amount: 295.45, date: new Date() },
+    { title: 'Car Insurance', amount: 295.45, date: new Date() },
+    { title: 'Car Insurance', amount: 295.45, date: new Date() },
+    { title: 'Car Insurance', amount: 295.45, date: new Date() },
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <ExpenseItem></ExpenseItem>
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        ></ExpenseItem>
+
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        ></ExpenseItem>
+
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        ></ExpenseItem>
+
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        ></ExpenseItem>
       </header>
     </div>
 
