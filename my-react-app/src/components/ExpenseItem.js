e3551cc3ad088c76bb6d66beefee0690b21d58bc
djhi12 <ido20002@byui.edu>
2023-08-24 14:19:42 +0800

    Initial commit

--- my-react-app/src/components/ExpenseItem.js
+++ my-react-app/src/components/ExpenseItem.js
@@ -1,8 +1,25 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
     return (
-        <div>
-            <h1 className="btn btn-primary">Expense Item</h1>
-            <p>Sample</p>
+        <div className="expense-item">
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+
+            <div className='expense-item_description'>
+                <h2>{props.title}</h2>
+            </div>
+
+            <div className='expense-item_price'>
+                <div>{props.amount}</div>
+            </div>
         </div>
     );
 }
