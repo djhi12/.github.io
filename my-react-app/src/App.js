8457ba43f045398c49d2405b0e54190df1a66688
djhi12 <ido20002@byui.edu>
2023-08-24 15:47:11 +0800

    Initial commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+// import ExpenseItem from './components/ExpenseItem';
 import Expenses from './components/Expenses';
 
 function App() {
-  const expenses = [
-    { title: 'Car Insurance', amount: 295.45, date: new Date() },
-    { title: 'Medical', amount: 495.45, date: new Date() },
-  ]
   return (
     <div className="App">
       <header className="App-header">
